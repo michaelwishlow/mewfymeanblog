4b7e4b397d01111ee0e8f057f9248cf60e4780e2
michaelwishlow <michael@pantheritconsulting.com>
2017-05-08 18:18:29 -0400

    added body-parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 app.post("/api/blogpost", createPost);
@@ -9,6 +12,7 @@ app.post("/api/blogpost", createPost);
 function createPost (req, res){
 	var post = req.body;
 	console.log(post);
+	res.json(post);
 }
 
 app.listen(3000);
