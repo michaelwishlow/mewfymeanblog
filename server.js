d538e7cc02a49f55bd3e8a28ed00235aeb220bde
michaelwishlow <michael@pantheritconsulting.com>
2017-05-08 19:19:28 -0400

    Add mongoose

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mewfymeanblog');
+
+var PostSchema = mongoose.Schema({
+	title: {type: String, required:true},
+	body: String,
+	tag: String,
+	posted: Date
+});
+
+var PostModel = mongoose.model("PostModel", PostSchema);
+
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -12,6 +26,7 @@ app.post("/api/blogpost", createPost);
 function createPost (req, res){
 	var post = req.body;
 	console.log(post);
+	PostModel.create(post);
 	res.json(post);
 }
 
